add0a5061d3d83ddf696da362e4051087b2599e6
Quentin Grimonprez <quentingrim@yahoo.fr>
2023-06-14 22:42:11 +0000

    version 4.1.5

--- R/MIXTCOMP_formatParameter.R
+++ R/MIXTCOMP_formatParameter.R
@@ -1,5 +1,5 @@
 # MixtComp version 4 - july 2019
-# Copyright (C) Inria - Université de Lille - CNRS 
+# Copyright (C) Inria - Université de Lille - CNRS
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -9,91 +9,89 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>
- 
+
 
 #' @title Format the model parameter
-#' 
+#'
 #' @description Format the model list for rmc/rmcMultiRun functions:
 #' - add paramStr when missing
 #' - ensure the list format of each element
-#' 
+#'
 #' @param model description of model used per variable
-#' 
+#'
 #' @return model as a list where each element is the model applied to a variable (list with elements type and paramStr)
-#' 
+#'
 #' @author Quentin Grimonprez
-#' 
+#'
 #' @export
-formatModel <- function(model)
-{
-  if(("matrix" %in% class(model)) || ("data.frame" %in% class(model)))
-  {
-    model = apply(model, 2, function(x) {
+formatModel <- function(model) {
+  if (("matrix" %in% class(model)) || ("data.frame" %in% class(model))) {
+    model <- apply(model, 2, function(x) {
       out <- list(type = as.character(x[1]))
-      if(length(x) >= 2)
-        out$paramStr = as.character(x[2])
-      
+      if (length(x) >= 2) {
+        out$paramStr <- as.character(x[2])
+      }
+
       out
     })
-  }else{
-    model = lapply(model, function(x){
-      if(!is.list(x))
-        x = list(type = as.character(x))
-      if(!("paramStr" %in% names(x)))
-        x$paramStr = ""
-      
+  } else {
+    model <- lapply(model, function(x) {
+      if (!is.list(x)) {
+        x <- list(type = as.character(x))
+      }
+      if (!("paramStr" %in% names(x))) {
+        x$paramStr <- ""
+      }
+
       return(x)
     })
   }
-  
+
   return(model)
 }
 
 
 #' @title Format the data parameter required by rmc
-#' 
+#'
 #' @description format data.frame or matrix in list of character
-#' 
+#'
 #' @param data data parameter as data.frame, matrix or list
-#' 
+#'
 #' @return data as a list of characters
-#' 
+#'
 #' @author Quentin Grimonprez
-#' 
+#'
 #' @export
-formatData <- function(data)
-{
-  if(is.data.frame(data) || is.matrix(data))
-  {
-    out <- lapply(1:ncol(data), function(i) as.character(data[,i]))
-    names(out) = colnames(data)
-  }else{
+formatData <- function(data) {
+  if (is.data.frame(data) || is.matrix(data)) {
+    out <- lapply(seq_len(ncol(data)), function(i) as.character(data[, i]))
+    names(out) <- colnames(data)
+  } else {
     out <- lapply(data, as.character)
   }
-  
+
   return(out)
 }
 
 
 #' @title Add the missing element to algo parameter
-#' 
-#' Add the missing element to algo parameter with default values
+#'
+#' @description Add the missing element to algo parameter with default values
 #'
 #' @param algo a list with the different algo parameters for rmc function
 #'
 #' @return algo parameter with all required elements (see \link{createAlgo} function)
 #'
 #' @author Quentin Grimonprez
-#' 
+#'
 #' @export
-completeAlgo <- function(algo)
-{
+completeAlgo <- function(algo) {
   algoDefault <- createAlgo()
-  
+
   missingNames <- setdiff(names(algoDefault), names(algo))
-  
+
   return(c(algo, algoDefault[missingNames]))
 }
--- R/PLOT_barplot.R
+++ R/PLOT_barplot.R
@@ -1,5 +1,5 @@
 # MixtComp version 4 - july 2019
-# Copyright (C) Inria - Université de Lille - CNRS 
+# Copyright (C) Inria - Université de Lille - CNRS
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -9,30 +9,36 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>
- 
+
 
 #' Barplot of the discriminative power of the variables
-#' 
+#'
 #' @details The discriminative power of variable j is defined by 1 - C(j)
 #' \deqn{C(j)=  -\sum_{k=1}^K \sum_{i=1}^n P(Z_i=k|x_{ij}) ln(P(Z_i=k|x_{ij})) / (n*\log(K))}
-#' 
-#' @param output object returned by \emph{mixtCompLearn} function from \emph{RMixtComp} or \emph{rmcMultiRun} function from \emph{RMixtCompIO}
-#' @param class NULL or a number of classes. If NULL, return the discrimative power of variables globally otherwise return the discrimative power of variables in the given class
+#'
+#' @param output object returned by \emph{mixtCompLearn} function from \emph{RMixtComp} or \emph{rmcMultiRun} function
+#' from \emph{RMixtCompIO}
+#' @param class NULL or a number of classes. If NULL, return the discrimative power of variables globally otherwise
+#' return the discrimative power of variables in the given class
 #' @param ylim vector of length 2 defining the range of y-axis
 #' @param pkg "ggplot2" or "plotly". Package used to plot
 #' @param ... arguments to be passed to plot_ly
-#' 
-#' @examples 
+#'
+#' @examples
 #' require(RMixtCompIO) # for learning a mixture model
-#' dataLearn <- list(var1 = as.character(c(rnorm(50, -2, 0.8), rnorm(50, 2, 0.8))),
-#'                   var2 = as.character(c(rnorm(50, 2), rpois(50, 8))))
-#'                   
-#' model <- list(var1 = list(type = "Gaussian", paramStr = ""),
-#'               var2 = list(type = "Poisson", paramStr = ""))
-#' 
+#' dataLearn <- list(
+#'   var1 = as.character(c(rnorm(50, -2, 0.8), rnorm(50, 2, 0.8))),
+#'   var2 = as.character(c(rnorm(50, 2), rpois(50, 8)))
+#' )
+#'
+#' model <- list(
+#'   var1 = list(type = "Gaussian", paramStr = ""),
+#'   var2 = list(type = "Poisson", paramStr = "")
+#' )
+#'
 #' algo <- list(
 #'   nClass = 2,
 #'   nInd = 100,
@@ -47,22 +53,22 @@
 #'   nStableCriterion = 10,
 #'   mode = "learn"
 #' )
-#' 
+#'
 #' resLearn <- rmcMultiRun(algo, dataLearn, model, nRun = 3)
-#' 
+#'
 #' # plot
 #' plotDiscrimVar(resLearn)
-#' 
+#'
 #' plotDiscrimVar(resLearn, class = 1)
-#' 
+#'
 #' @seealso \code{\link{computeDiscrimPowerVar}}
-#' 
+#'
 #' @author Matthieu MARBAC
 #' @family plot
 #' @export
-plotDiscrimVar <- function(output, class = NULL, ylim = c(0, 1), pkg = c("ggplot2", "plotly"), ...){
-  pkg = match.arg(pkg)
-  
+plotDiscrimVar <- function(output, class = NULL, ylim = c(0, 1), pkg = c("ggplot2", "plotly"), ...) {
+  pkg <- match.arg(pkg)
+
   ## Get information
   # names of variables
   namesVbles <- names(output$variable$type)[names(output$variable$type) != "z_class"]
@@ -70,46 +76,53 @@ plotDiscrimVar <- function(output, class = NULL, ylim = c(0, 1), pkg = c("ggplot
   # discriminative power (1 - Cj), saved at slot pvdiscrimclasses of JSON file
   pvDiscrim <- round(computeDiscrimPowerVar(output, class), 2)
   # Full names and type of the variables
-  textMous <- paste(namesVbles, '<br>', as.character(output$variable$type[-1]))
-  
-  ## Variables are sorted by descreasing order of their discriminative power
+  textMous <- paste(namesVbles, "<br>", as.character(output$variable$type[-1]))
+
+  ## Variables are sorted by decreasing order of their discriminative power
   ## Character must be convert in factor (otherwise variables are plotted with alphabetic order)
   orderVbles <- order(pvDiscrim, decreasing = TRUE)
-  pvDiscrim  <- pvDiscrim[orderVbles]
-  namesVbles <- factor(namesVbles[orderVbles], levels=namesVbles[orderVbles])
-  namesShort <- factor(namesShort[orderVbles], levels=namesShort[orderVbles])
-  textMous   <- factor(textMous[orderVbles], levels=textMous[orderVbles])
-  
+  pvDiscrim <- pvDiscrim[orderVbles]
+  namesVbles <- factor(namesVbles[orderVbles], levels = namesVbles[orderVbles])
+  namesShort <- factor(namesShort[orderVbles], levels = namesShort[orderVbles])
+  textMous <- factor(textMous[orderVbles], levels = textMous[orderVbles])
+
   plotTitle <- ifelse(is.null(class),
-                      "Discriminative level of the variables",
-                      paste0("Discriminative level of the variables in class ", class))
-  
+    "Discriminative level of the variables",
+    paste0("Discriminative level of the variables in class ", class)
+  )
+
   ## Barplot
   visuVbles <- switch(pkg,
-                      "plotly" = barplotly(pvDiscrim, namesShort, main = plotTitle, ylim = ylim, text = textMous, ...),
-                      "ggplot2" = ggbarplot(pvDiscrim, namesShort, main = plotTitle, ylim = ylim, col.text = "black")) 
+    "plotly" = barplotly(pvDiscrim, namesShort, main = plotTitle, ylim = ylim, text = textMous, ...),
+    "ggplot2" = ggbarplot(pvDiscrim, namesShort, main = plotTitle, ylim = ylim, col.text = "black")
+  )
   visuVbles
 }
 
 #' Barplot of the discriminative power of the classes
-#' 
+#'
 #' @details The discriminative power of class k is defined by 1 - D(k)
 #' \deqn{D(k) =  -\sum_{i=1}^n P(Z_i=k|x_i) \log(P(Z_i=k|x_i)) / (n*\exp(-1))}
-#' 
-#' @param output object returned by \emph{mixtCompLearn} function from \emph{RMixtComp} or \emph{rmcMultiRun} function from \emph{RMixtCompIO}
+#'
+#' @param output object returned by \emph{mixtCompLearn} function from \emph{RMixtComp} or \emph{rmcMultiRun} function
+#' from \emph{RMixtCompIO}
 #' @param ylim vector of length 2 defining the range of y-axis
 #' @param pkg "ggplot2" or "plotly". Package used to plot
 #' @param ... arguments to be passed to plot_ly
-#' 
-#' @examples 
-#' 
+#'
+#' @examples
+#'
 #' require(RMixtCompIO) # for learning a mixture model
-#' dataLearn <- list(var1 = as.character(c(rnorm(50, -2, 0.8), rnorm(50, 2, 0.8))),
-#'                   var2 = as.character(c(rnorm(50, 2), rpois(50, 8))))
-#'                   
-#' model <- list(var1 = list(type = "Gaussian", paramStr = ""),
-#'               var2 = list(type = "Poisson", paramStr = ""))
-#' 
+#' dataLearn <- list(
+#'   var1 = as.character(c(rnorm(50, -2, 0.8), rnorm(50, 2, 0.8))),
+#'   var2 = as.character(c(rnorm(50, 2), rpois(50, 8)))
+#' )
+#'
+#' model <- list(
+#'   var1 = list(type = "Gaussian", paramStr = ""),
+#'   var2 = list(type = "Poisson", paramStr = "")
+#' )
+#'
 #' algo <- list(
 #'   nClass = 2,
 #'   nInd = 100,
@@ -124,56 +137,63 @@ plotDiscrimVar <- function(output, class = NULL, ylim = c(0, 1), pkg = c("ggplot
 #'   nStableCriterion = 10,
 #'   mode = "learn"
 #' )
-#' 
+#'
 #' resLearn <- rmcMultiRun(algo, dataLearn, model, nRun = 3)
-#' 
+#'
 #' plotDiscrimClass(resLearn)
-#' 
+#'
 #' @seealso \code{\link{computeDiscrimPowerClass}}
-#' 
+#'
 #' @author Matthieu MARBAC
 #' @family plot
 #' @export
-plotDiscrimClass <- function(output, ylim = c(0, 1), pkg = c("ggplot2", "plotly"), ...){
-  pkg = match.arg(pkg)
-  
+plotDiscrimClass <- function(output, ylim = c(0, 1), pkg = c("ggplot2", "plotly"), ...) {
+  pkg <- match.arg(pkg)
+
   ## Get information
   # names of classes
-  if(is.null(output$algo$dictionary$z_class))
-    namesClass <- paste0("Class ", 1:output$algo$nClass)
-  else
+  if (is.null(output$algo$dictionary$z_class)) {
+    namesClass <- paste0("Class ", seq_len(output$algo$nClass))
+  } else {
     namesClass <- output$algo$dictionary$z_class$old
+  }
   # discriminative power (1 - Dk), saved at slot pvdiscrimvbles of JSON file
-  pvDiscrim <-   round(1 - (-colMeans(log(output$variable$data$z_class$stat**output$variable$data$z_class$stat)) / exp(-1)), 2)
-  
-  ## Classes are sorted by descreasing order of their discriminative power
+  pvDiscrim <- round(1 - (-colMeans(log(output$variable$data$z_class$stat**output$variable$data$z_class$stat)) / exp(-1)), 2)
+
+  ## Classes are sorted by decreasing order of their discriminative power
   ## Character must be convert in factor (otherwise alphabetic order is considered)
   orderClass <- order(pvDiscrim, decreasing = TRUE)
-  namesClass <- factor(namesClass[orderClass], levels=namesClass[orderClass])
+  namesClass <- factor(namesClass[orderClass], levels = namesClass[orderClass])
   pvDiscrim <- pvDiscrim[orderClass]
   ## Barplot
   visuClass <- switch(pkg,
-                      "plotly" = barplotly(pvDiscrim, namesClass, main = "Discriminative level of the classes", ylim = ylim, ...),
-                      "ggplot2" = ggbarplot(pvDiscrim, namesClass, main = "Discriminative level of the classes", ylim = ylim, col.text = "black"))
-  
+    "plotly" = barplotly(pvDiscrim, namesClass, main = "Discriminative level of the classes", ylim = ylim, ...),
+    "ggplot2" = ggbarplot(pvDiscrim, namesClass, main = "Discriminative level of the classes", ylim = ylim, col.text = "black")
+  )
+
   visuClass
 }
 
 
-#' Plot the mixture's proportions 
+#' Plot the mixture's proportions
 #'
-#' @param output object returned by \emph{mixtCompLearn} function from \emph{RMixtComp} or \emph{rmcMultiRun} function from \emph{RMixtCompIO}
+#' @param output object returned by \emph{mixtCompLearn} function from \emph{RMixtComp} or \emph{rmcMultiRun} function
+#' from \emph{RMixtCompIO}
 #' @param pkg "ggplot2" or "plotly". Package used to plot
 #' @param ... arguments to be passed to plot_ly
-#' 
-#' @examples 
+#'
+#' @examples
 #' require(RMixtCompIO) # for learning a mixture model
-#' dataLearn <- list(var1 = as.character(c(rnorm(50, -2, 0.8), rnorm(50, 2, 0.8))),
-#'                   var2 = as.character(c(rnorm(50, 2), rpois(50, 8))))
-#'                   
-#' model <- list(var1 = list(type = "Gaussian", paramStr = ""),
-#'               var2 = list(type = "Poisson", paramStr = ""))
-#' 
+#' dataLearn <- list(
+#'   var1 = as.character(c(rnorm(50, -2, 0.8), rnorm(50, 2, 0.8))),
+#'   var2 = as.character(c(rnorm(50, 2), rpois(50, 8)))
+#' )
+#'
+#' model <- list(
+#'   var1 = list(type = "Gaussian", paramStr = ""),
+#'   var2 = list(type = "Poisson", paramStr = "")
+#' )
+#'
 #' algo <- list(
 #'   nClass = 2,
 #'   nInd = 100,
@@ -188,65 +208,75 @@ plotDiscrimClass <- function(output, ylim = c(0, 1), pkg = c("ggplot2", "plotly"
 #'   nStableCriterion = 10,
 #'   mode = "learn"
 #' )
-#' 
+#'
 #' resLearn <- rmcMultiRun(algo, dataLearn, model, nRun = 3)
-#' 
+#'
 #' # plot
 #' plotProportion(resLearn)
-#' 
-#' 
+#'
 #' @family plot
 #' @author Quentin Grimonprez
 #' @export
-plotProportion <- function(output, pkg = c("ggplot2", "plotly"), ...)
-{
-  pkg = match.arg(pkg)
-  
-  ylimProportion <- min(max(output$variable$param$z_class$stat[,1]) + 0.1, 1)
-  if(is.null(output$algo$dictionary$z_class))
-    label <- paste0("Class ", 1:output$algo$nClass)
-  else
+plotProportion <- function(output, pkg = c("ggplot2", "plotly"), ...) {
+  pkg <- match.arg(pkg)
+
+  ylimProportion <- min(max(output$variable$param$z_class$stat[, 1]) + 0.1, 1)
+  if (is.null(output$algo$dictionary$z_class)) {
+    label <- paste0("Class ", seq_len(output$algo$nClass))
+  } else {
     label <- output$algo$dictionary$z_class$old
-    
-  
-  p <- switch(pkg, 
-              "ggplot2" = ggbarplot(output$variable$param$z_class$stat[,1], label, main = "Proportion", xlab = "", ylab = "", ylim = c(0, ylimProportion), col.text = "black"),
-              "plotly" = barplotly(output$variable$param$z_class$stat[,1], label, main = "Proportion", xlab = "", ylab = "", ylim = c(0, ylimProportion), text = NULL, ...))
-  
+  }
+
+
+  p <- switch(pkg,
+    "ggplot2" = ggbarplot(
+      output$variable$param$z_class$stat[, 1], label, main = "Proportion", xlab = "", ylab = "",
+      ylim = c(0, ylimProportion), col.text = "black"
+    ),
+    "plotly" = barplotly(
+      output$variable$param$z_class$stat[, 1], label, main = "Proportion", xlab = "",
+      ylab = "", ylim = c(0, ylimProportion), text = NULL, ...
+    )
+  )
+
   p
 }
 
 # @author Quentin Grimonprez
-barplotly <- function(value, label, main, xlab = "", ylab = "", ylim = c(0, 1), text = NULL, ...)
-{
-  p <- plot_ly(x = label,
-               y = value,
-               type = "bar",
-               text = text
+barplotly <- function(value, label, main, xlab = "", ylab = "", ylim = c(0, 1), text = NULL, ...) {
+  p <- plot_ly(
+    x = label,
+    y = value,
+    type = "bar",
+    text = text
   ) %>%
-    layout(title = main,
-           xaxis = list(title = xlab),
-           yaxis = list(title = ylab, range = ylim),
-           annotations = list(x = label,
-                              y = value,
-                              text = round(value, 2),
-                              xanchor = "center",
-                              yanchor = "bottom",
-                              showarrow = FALSE)
+    layout(
+      title = main,
+      xaxis = list(title = xlab),
+      yaxis = list(title = ylab, range = ylim),
+      annotations = list(
+        x = label,
+        y = value,
+        text = round(value, 2),
+        xanchor = "center",
+        yanchor = "bottom",
+        showarrow = FALSE
+      )
     )
   p
 }
 
 # @author Matthieu Marbac
-ggbarplot <- function(value, label, main, xlab = "", ylab = "", ylim = c(0, 1), col.text = "white")
-{
-  df = data.frame(var = factor(label, levels = label), discrim = value, roundedDiscrim = round(value, 2))
+ggbarplot <- function(value, label, main, xlab = "", ylab = "", ylim = c(0, 1), col.text = "white") {
+  df <- data.frame(var = factor(label, levels = label), discrim = value, roundedDiscrim = round(value, 2))
   ggplot(data = df, aes_string(x = "var", y = "discrim")) +
     geom_bar(stat = "identity") +
     geom_text(aes_string(label = "roundedDiscrim"), vjust = -0.5, color = col.text, size = 5) +
     theme_minimal() +
     ylim(ylim[1], ylim[2]) +
     labs(title = main, x = xlab, y = ylab) +
-    theme(legend.position = "none",
-          axis.text.x = element_text(angle = 315, hjust = 0))
+    theme(
+      legend.position = "none",
+      axis.text.x = element_text(angle = 315, hjust = 0)
+    )
 }
